1ded4e51756fe4894eb8f259c79a0a90fdc46375
Nurullayev Rashad <rashadfn@code.local>
2023-09-16 14:04:04 +0400

    done

--- app.js
+++ app.js
@@ -1,61 +1,99 @@
 const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(express.json());
 
-app.get("/", (req, res) => res.type('html').send(html));
+let db = [
+  {
+    actor_id: 1,
+    first_name: "PENELOPE",
+    last_name: "GUINESS",
+    last_update: "2006-02-15T00:34:33.000Z",
+  },
+  {
+    actor_id: 2,
+    first_name: "NICK",
+    last_name: "WAHLBERG",
+    last_update: "2006-02-15T00:34:33.000Z",
+  },
+  {
+    actor_id: 3,
+    first_name: "ED",
+    last_name: "CHASE",
+    last_update: "2006-02-15T00:34:33.000Z",
+  },
+];
+let connection = mysql.createConnection({
+  host: "b1pbtsiw0jlyozs86myr-mysql.services.clever-cloud.com",
+  user: "ugkdgoxx9rlzpauc",
+  password: "iZXOCGGrrRWtpZ0Ux3hY",
+  database: "b1pbtsiw0jlyozs86myr",
+});
+// app.get("/student", function (req, res) {
+//   res.send(db);
+// });
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// // api get method
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.get("/", function (req, res) {
+  connection.query("select * from onlar", function (err, result, fields) {
+    //   console.log(err);
+    //   console.log(result);
+    //   console.log(fields);
+    res.send(result);
+  });
+});
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+// app.get("/student", (req, res) => {
+//   res.send(db);
+// });
+
+// id get
+app.get("/onlar/:id", (req, res) => {
+  const elem = req.params;
+  // sql id get method
+  connection.query("select * from onlar", function (err, result, fields) {
+    // console.log(result);
+    for (let i = 0; i < result.length; i++) {
+      if (elem.id == result[i].ID) {
+        res.send(result[i]);
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    }
+  });
+});
+// delete method
+app.delete("/onlar/:id", (req, res) => {
+  const elem = req.params.id;
+  const silininenElementArray = db.filter(
+    (element) => element.actor_id != elem
+  );
+  connection.query(
+    `DELETE FROM onlar WHERE ID=${elem}`,
+    function (err, result, fields) {
+      console.log(result);
+    }
+  );
+});
+
+// post method
+app.post("/onlar/", (req, res) => {
+  let obj = req.body;
+  connection.query(
+    `INSERT INTO onlar (ID, ad, soyad)
+    VALUES ("${obj.ID}", "${obj.ad}", "${obj.soyad}")`,
+    function (err, result, fields) {
+      //   console.log(result);
+      //   app.get("/student", function (req, res) {
+      //     res.send(result);
+      //   });
+    }
+  );
+  connection.query("select * from onlar", function (err, result, fields) {
+    //   console.log(err);
+    console.log(result);
+    res.send(result);
+  });
+});
+
+app.listen(process.env.PORT || 3000);
